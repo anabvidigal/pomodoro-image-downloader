3fdca344f092e514bcfb0193bfa4ff1ae9695a63
anabvidigal <anabvidigal@gmail.com>
2023-11-07 15:27:47 -0300

    added capability to iterate through list and customize save folder names

--- script.py
+++ script.py
@@ -1,22 +1,23 @@
-def main():
-    import requests, os, bs4
-    from selenium import webdriver, common
-    from selenium.webdriver.common.by import By
+import requests, os, bs4
+from selenium import webdriver, common
+from selenium.webdriver.common.by import By
 
-    # Open Browser to photo-sharing site
-    url = "https://www.flickr.com/search/?text=" # starting url
-    os.makedirs("images", exist_ok=True) # store images in ./images
+list = [
+    "Tomato",
+    "Cherry Tomato",
+    "Tomato Soup",
+    "Bloody Mary",
+    "Bruschetta",
+    "Pizza",
+    "Caprese",
+    "Tomato sauce"
+]
 
-    browser = webdriver.Firefox()
-    browser.implicitly_wait(10) # seconds
-
-    # Search for category of photos
-    browser.get(url + "Tomato")
-
+def download_images(browser, item, amount):
     # Download all images
     try:
         imageElems = browser.find_elements(By.CSS_SELECTOR, 'a.overlay')
-        for element in imageElems[:6]:
+        for element in imageElems[:amount]:
             downloadUrl = element.get_attribute("href") + "sizes/o/"
             res = requests.get(downloadUrl)
             res.raise_for_status()
@@ -29,11 +30,16 @@ def main():
             else:
                 imageUrl = imageElem[2].get("src")
 
+                # Skip gifs
+                if imageUrl.endswith(".gif"):
+                    print(f"Skipped .gif file: {imageUrl}")
+                    continue
+
                 # Save image to ./images
                 res = requests.get(imageUrl)
                 res.raise_for_status()
 
-                imageFile = open(os.path.join("images", os.path.basename(imageUrl)), "wb")
+                imageFile = open(os.path.join(f"images/{item}", os.path.basename(imageUrl)), "wb")
                 for chunk in res.iter_content(100000):
                     imageFile.write(chunk)
                 imageFile.close()
@@ -41,7 +47,24 @@ def main():
     except common.exceptions.NoSuchElementException as err:
         print("Unable to locate element: %s" % err)
 
-    browser.close()
+def main():
+
+    # Open Browser to photo-sharing site
+    url = "https://www.flickr.com/search/?text=" # starting url
+
+    browser = webdriver.Firefox()
+    browser.implicitly_wait(10) # seconds
+
+    for item in list:
+        # Search for category of photos
+        browser.get(url + item)
+
+        # store images in ./images
+        os.makedirs(f"images/{item}", exist_ok=True)
+
+        download_images(browser, item, amount=8)
+
+    browser.quit()
 
 if __name__ == '__main__':
     main()
